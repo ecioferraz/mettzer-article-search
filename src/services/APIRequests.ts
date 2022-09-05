2407410da539092511f2feab2d9dcfe986c79011
Écio Ferraz <ecioferraz@hotmail.com>
2022-09-05 19:28:36 -0300

    refactor: APIRequests environment variable

--- src/services/APIRequests.ts
+++ src/services/APIRequests.ts
@@ -4,11 +4,14 @@ const baseURL = 'https://core.ac.uk:443/api-v2/';
 
 const api = axios.create({ baseURL });
 
-const getData = async (query: string, page = 1) =>
+const apiKey = process.env.REACT_APP_API_KEY;
+
+const getData = async (query: string, page: number) =>
   api.get(`/search/${query}`, {
     params: {
       page,
-      apiKey: process.env.API_KEY,
+      apiKey,
+      pageSize: 10,
     },
   });
 
