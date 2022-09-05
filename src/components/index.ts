20dce08d601080e46e8bf5d8a4e3788eb125b2f7
Écio Ferraz <ecioferraz@hotmail.com>
2022-09-05 15:56:29 -0300

    feat: ExternalLink to components index

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,10 +1,12 @@
 import Button from './Button';
+import ExternalLink from './ExternalLink';
 import RedirectLink from './RedirectLink';
 import TextCard from './TextCard';
 import TextInput from './TextInput';
 
 export {
   Button,
+  ExternalLink,
   RedirectLink,
   TextCard,
   TextInput,
