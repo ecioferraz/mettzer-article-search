5c8e3dfb199f22388ec4f85bdef59b741ae18d12
Écio Ferraz <ecioferraz@hotmail.com>
2022-09-05 15:56:10 -0300

    style: TextCard props order

--- src/components/TextCard/TextCard.tsx
+++ src/components/TextCard/TextCard.tsx
@@ -3,8 +3,8 @@ import ITextCard from './ITextCard';
 
 export default function TextCard({
   as: Tag = 'span',
-  text,
   className,
+  text,
 }: ITextCard) {
   return <Tag className={className}>{text}</Tag>;
 }
