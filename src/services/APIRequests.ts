84e6759db6c840fa8f129edfc81d5c973b861942
Écio Ferraz <ecioferraz@hotmail.com>
2022-09-08 18:59:06 -0300

    refactor: change APIRequest method to post for sorting purposes

--- src/services/APIRequests.ts
+++ src/services/APIRequests.ts
@@ -1,18 +1,17 @@
 import axios from 'axios';
 
+const pageSize = 10;
+
 const baseURL = 'https://core.ac.uk:443/api-v2/';
 
 const api = axios.create({ baseURL });
 
-const apiKey = process.env.REACT_APP_API_KEY;
+const apiKey = process.env.REACT_APP_API_KEY as string;
 
 const getData = async (query: string, page: number) =>
-  api.get(`/search/${query}`, {
-    params: {
-      page,
-      apiKey,
-      pageSize: 10,
-    },
-  });
+  api.post(
+    `/search?apiKey=${apiKey}`,
+    [{query, page, pageSize}],
+  );
 
 export default getData;
